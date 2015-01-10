b75e946abbfb323509ef6a9511a546e693cd9989
ephe-meral <j.appel.online@gmail.com>
2015-01-11 00:15:02 +0100

    adds tests for the maps macros

--- test/entice/area/maps_test.exs
+++ test/entice/area/maps_test.exs
@@ -4,11 +4,31 @@ defmodule Entice.Area.MapsTest do
 
   defmap SomeMap
 
+
+  # Create some maps and...
+  defmodule SomeMaps do
+    use Entice.Area.Maps
+
+    defmap SomeMap
+    defmap SomeOtherMap
+  end
+  # ...test if the using works
+  defmodule UsingSomeMaps do
+    use SomeMaps
+
+    def test, do: SomeMap.name
+  end
+
+
   test "map name" do
     assert SomeMap.name == SomeMap
   end
 
   test "map underscore name" do
     assert SomeMap.underscore_name == "some_map"
+  end
+
+  test "test __using__ macros compilation" do
+    assert UsingSomeMaps.test == SomeMaps.SomeMap.name
   end
 end
