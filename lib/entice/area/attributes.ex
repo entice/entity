b1fbc86fa9eb85c9472a149609baa1d6f71f43a0
ephe-meral <j.appel.online@gmail.com>
2015-01-18 23:09:36 +0100

    adds skillbar and movement

--- lib/entice/area/attributes.ex
+++ lib/entice/area/attributes.ex
@@ -6,13 +6,17 @@ defmodule Entice.Area.Attributes do
       alias Entice.Area.Geom.Coord
       alias Entice.Area.Attributes.Name
       alias Entice.Area.Attributes.Position
+      alias Entice.Area.Attributes.Movement
       alias Entice.Area.Attributes.Appearance
+      alias Entice.Area.Attributes.SkillBar
     end
   end
 
   defmodule Name, do: defstruct(name: "Hansus Wurstus")
 
   defmodule Position, do: defstruct(pos: %Coord{})
+
+  defmodule Movement, do: defstruct(goal: %Coord{}, type: 9, speed: 1.0)
 
   defmodule Appearance, do: defstruct(
     profession: 1,
@@ -23,4 +27,6 @@ defmodule Entice.Area.Attributes do
     hair_color: 0,
     hairstyle: 7,
     face: 30)
+
+  defmodule SkillBar, do: defstruct(slots: %{})
 end
