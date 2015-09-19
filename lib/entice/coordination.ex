eea602f2cae18c3dae3dab5db772cd9c6c540247
ephe-meral <j.appel.online@gmail.com>
2015-09-19 22:27:48 +0200

    minor unused var/alias fix

--- lib/entice/coordination.ex
+++ lib/entice/coordination.ex
@@ -36,7 +36,7 @@ defmodule Entice.Entity.Coordination do
 
   @doc "This might fail silently. This is because entities might have died by the time we message them"
   def notify(entity, message) when is_pid(entity), do: send(entity, message)
-  def notify(nil, message),                        do: {:error, :entity_nil}
+  def notify(nil, _message),                       do: {:error, :entity_nil}
   def notify(entity_id, message),                  do: notify(Entity.get(entity_id), message)
 
 
--- lib/entice/entity/server.ex
+++ lib/entice/entity/server.ex
@@ -1,5 +1,4 @@
 defmodule Entice.Entity.Server do
-  alias Entice.Entity
   alias Entice.Utils.SyncEvent
 
   def start_link(args),
