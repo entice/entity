a2a68db6126c4008c472146ea41b2943eba54616
ephe-meral <j.appel.online@gmail.com>
2015-11-02 15:21:52 +0100

    make suicide_test an elixir script file

--- lib/entice/entity/suicide.ex
+++ lib/entice/entity/suicide.ex
@@ -6,6 +6,7 @@ defmodule Entice.Entity.Suicide do
   (E.g. if you are referring to the entity indirectly via coordination etc.
   """
   alias Entice.Entity
+  alias Entice.Entity.Coordination
   alias Entice.Entity.Suicide
 
 
